27f80878c0e08437c408e8f8220f222ded747905
gh0o5t <gh0o5t@protonmail.com>
2021-01-17 17:24:02 +0100

    Network info

--- blocks.h
+++ blocks.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ",      "memory.sh",          	30,	            	0},
-	{"  ",      "ip.sh",               	30,	            	0},
-	{"  ",       "date.sh",				5,	            	0},
+	{"  ",      "memory.sh",          	    10,	            	0},
+	{"",         "network.sh",               	10,	            	0},
+	{"",         "battery.sh",             	10,	            	0},
+	{"  ",      "date.sh",			    	5,	            	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
