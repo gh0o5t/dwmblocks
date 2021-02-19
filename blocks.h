a9ef116108bfb3573fffcdbfe7177d57f623ded3
gh0o5t <gh0o5t@protonmail.com>
2021-02-19 22:05:26 +0100

    volume and cmdlength

--- blocks.h
+++ blocks.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ",      "layout.sh",          	     0,	            	1},
+	{"",         "volume.sh",          	     5,	            	2},
+	{"  ",      "layout.sh",          	   120,	            	1},
 	{"  ",      "memory.sh",          	    10,	            	0},
 	{"",         "network.sh",            	10,	            	0},
 	{"",         "battery.sh",             	10,	            	0},
-	{"  ",      "date.sh",			    	5,	            	0},
+	{"  ",      "date.sh",			       	 5,	            	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
